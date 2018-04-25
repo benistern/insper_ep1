cb9c0dfe5bac8b249f4b20fa9e4073359fed07fd
felipe valente <felipevalente2901@gmail.com>
2018-04-25 18:51:55 -0300

    ovo de codorna3

---
+++ ep1_parte2.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 20 11:10:38 2018
+
+@author: felip
+"""
+import json
+
+with open('ep1.json','r') as arquivo:
+    conteudo = arquivo.read()
+    controle_estoque = json.loads(conteudo)
+
+
+controle_estoque = {}
+aberto = True
+while aberto:
+    print("0 - sair")
+    print("1 - adicionar item")
+    print("2 - remover item")
+    print("3 - alterar item")
+    print("4 - imprimir estoque")
+    # aqui o usuario escolhe a acao
+    
+    a = int(input("Faça sua escolha: "))
+    
+    if a == 0:
+        print("Até mais")
+        aberto = False
+  
+    # adicionar item
+    elif a == 1:
+        b = str(input("Nome do produto: "))
+        if b in controle_estoque:
+            print("Produto já cadastrado")
+        else:
+            c = int(input("Quantidade inicial: "))
+            if c < 0:
+                print("A quantidade inicial não pode ser negativa")
+            if b not in controle_estoque and c>0:
+                controle_estoque[b]= {"quantidade": c}  
+    
+    #remover item        
+    elif a == 2:
+        d = str(input("Nome do produto: "))
+        if d not in controle_estoque:
+            print("Elemento não encontrado")
+        else:
+            del controle_estoque[d]
+    
+    
+   #alterar item
+    elif a == 3:
+        e = str(input("Nome do produto: "))
+        if e not in controle_estoque:
+            print("Elemento não encontrado")
+        else:
+            f = int(input("Quantidade: "))
+            controle_estoque[e]["quantidade"] += f
+                        
+            
+            
+    #imprimir estoque
+    elif a==4:
+        for i in controle_estoque:
+            print(str(i)+(": ")+str(controle_estoque[i]["quantidade"]))
+    
+
+    
+with open('ep1.json', 'w') as arquivo2:
+    arquivo2.write(json.dumps(controle_estoque))
+    
+    
+    
+    
+    
+        
+    
+            
+        
+    
